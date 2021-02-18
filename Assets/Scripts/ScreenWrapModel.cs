71393a07712d349b51f998526dbb43c284f29492
CCapslock <53517856+CCapslock@users.noreply.github.com>
2021-02-18 05:05:22 +0300

    что то настроил

--- Assets/Scripts/ScreenWrapModel.cs
+++ Assets/Scripts/ScreenWrapModel.cs
@@ -152,15 +152,7 @@ public class ScreenWrapModel : MonoBehaviour
 		ScreenNum = 0;
 		_combiningCounter = 1;
 
-		if (new Rect(0, HeightOfGameScreen * 100 - HeightOfGameScreen * 100 / _amountOfDividing * (ScreenNum + 1), 2, 2).y 
-			> new Rect(0, HeightOfGameScreen * 100 - HeightOfGameScreen * 100 / _amountOfDividing * (ScreenNum + 1), 2, 2).y)
-		{
-			_isCountingFromUpToDown = true;
-		}
-		else
-		{
-			_isCountingFromUpToDown = false;
-		}
+		_isCountingFromUpToDown = false;
 
 		//Rect __rect = new Rect(0,HeightOfGameScreen * 100 / _amountOfDividing * (ScreenNum + 1), 2, 2);
 		//Debug.Log("__rect.y = " + __rect.y);
@@ -289,17 +281,16 @@ public class ScreenWrapModel : MonoBehaviour
 				Texture2D renderResult = new Texture2D(renderTexture.width, renderTexture.height / _amountOfDividing, TextureFormat.RGBA32, false);
 
 				Rect __rect = new Rect(0, HeightOfGameScreen * 100 - HeightOfGameScreen * 100 / _amountOfDividing * (ScreenNum + 1), renderTexture.width, renderTexture.height / _amountOfDividing);
-				Debug.Log("__rect.height" +
-					" = " + __rect.height);
+				//Debug.Log("__rect.height" +" = " + __rect.height);
 				if (_isCountingFromUpToDown)
 				{
-					_rect = new Rect(0, 200, renderTexture.width, renderTexture.height / _amountOfDividing);
-					//_rect = new Rect(0, HeightOfGameScreen * 100 - HeightOfGameScreen * 100 / _amountOfDividing * (ScreenNum + 1 ), renderTexture.width, renderTexture.height / _amountOfDividing);
+					//_rect = new Rect(0, 200, renderTexture.width, renderTexture.height / _amountOfDividing);
+					_rect = new Rect(0, HeightOfGameScreen * 100 - HeightOfGameScreen * 100 / _amountOfDividing * (ScreenNum + 1 ), renderTexture.width, renderTexture.height / _amountOfDividing);
 				}
 				else
 				{
-					_rect = new Rect(0, 200, renderTexture.width, renderTexture.height / _amountOfDividing);
-					//_rect = new Rect(0, HeightOfGameScreen * 100 / _amountOfDividing * (ScreenNum + 1), renderTexture.width, renderTexture.height / _amountOfDividing);
+					//_rect = new Rect(0, 200, renderTexture.width, renderTexture.height / _amountOfDividing);
+					_rect = new Rect(0, HeightOfGameScreen * 100 / _amountOfDividing * (ScreenNum + 1), renderTexture.width, renderTexture.height / _amountOfDividing);
 				}
 				renderResult.ReadPixels(_rect, 0, 0, false);
 				renderResult.Apply();
